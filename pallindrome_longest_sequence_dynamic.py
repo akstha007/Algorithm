c34c4daf313b87d7ebcc384fa2096e3ecdbc3441
akstha007 <cs.ashok007@gmail.com>
2018-03-12 00:01:41 -0500

    Longest Pallindrome sequence: dynamic - print subarray

--- pallindrome_longest_sequence_dynamic.py
+++ pallindrome_longest_sequence_dynamic.py
@@ -1,9 +1,18 @@
 #!/bin/python3
 
+def get_substring(L, i, j):
+    if L[i][j] > 0:
+        if L[i][j] == L[i + 1][j]:
+            get_substring(L, i + 1, j)
+        elif L[i][j] == L[i][j-1]:
+            get_substring(L, i, j-1)
+        else:
+            result_substring[i] = test_str[i]
+            result_substring[j] = test_str[j]
+            get_substring(L, i + 1, j - 1)
+
 
 def long_sequence_dynamic(str):
-    n = len(str)
-    L = [[0 for _ in range(n)] for _ in range(n)]
 
     for i in range(n):
         L[i][i] = 1
@@ -18,15 +27,27 @@ def long_sequence_dynamic(str):
             else:
                 L[i][j] = max(L[i + 1][j], L[i][j - 1])
 
-    return L[0][n-1]
+    for row in L:
+        print(row)
+
+    return L[0][n - 1]
+
+
+test_str = "characters"
+n = len(test_str)
+L = [[0 for _ in range(n)] for _ in range(n)]
+
+result_substring = ["" for _ in range(n)]
 
 
 def main():
     print("Longest Pallindrome sequence Problem: Dynamic solution")
-    test_str = "character"
-
+    print(list(test_str))
     result = long_sequence_dynamic(test_str)
     print("Result: {}".format(result))
+
+    get_substring(L, 0, n - 1)
+    print("".join(result_substring))
 
 
 if __name__ == "__main__":
